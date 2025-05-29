ccea7a40d4fd544c8fbb5de574cb1c1db438d851
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-05-29 22:58:38 +0530

    lecture 2 class room

---
+++ lecture 2/conditionalStatement.js
@@ -0,0 +1,44 @@
+// let age = 16;
+
+// if (age >= 18) {
+//   console.log("You are an adult.");
+// } else {
+//   console.log("You are a minor.");
+// }
+
+// let num = 18;
+
+// if (num % 2 === 0) {
+//   console.log("The number is even.");
+// } else {
+//   console.log("The number is odd.");
+// }
+
+// else -if statements
+
+// if (age < 18) {
+//   console.log("junior");
+// } else if (age > 60) {
+//   console.log("senior");
+// } else {
+//   console.log("adult");
+// }
+
+// const result = age > 18 ? "adult" : "not adult";
+// console.log(result);
+
+// age > 18 ? console.log("adult") : console.log("not adult");
+
+const expr = "papayaaaaaaas";
+
+switch (expr) {
+  case "orange":
+    console.log("orange are $0.59 a pound");
+    break;
+  case "mango":
+  case "papayas":
+    console.log("mangoes and papayas are $2.79 a pound");
+    break;
+  default:
+    console.log("Sorry, we are out of $(expr).");
+}
---
+++ lecture 2/main.js
@@ -0,0 +1,26 @@
+let a = 6;
+let b = 5;
+
+// const sum = a + b;
+// console.log(`The sum of ${a} and ${b} is ${sum}.`);
+// const product = a * b;
+// console.log(sum);
+// console.log(product);
+// console.log("The sum of a and b is ", sum);
+
+// const c = b % a;
+// console.log(c);
+
+// const c = a ** b;
+// console.log(c);
+// let d = a++;
+// let e = ++a;
+// console.log(d);
+// console.log(e);
+
+// console.log("a===b", a === b);
+
+let cond1 = a > b;
+let cond2 = a === 8;
+
+console.log("cond1 && cond2", cond1 || cond2);
