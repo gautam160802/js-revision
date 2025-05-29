c0f495b783972578cbf839e73fe2402e99c90842
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-05-29 17:17:29 +0530

    complete lecture 1

---
+++ lecture 1/practice1.js
@@ -0,0 +1,10 @@
+// create a const object called "product" to store information about a product
+
+const product = {
+  name: "parker jotter standard CT ball pen",
+  price: " rs150",
+  category: "stationery",
+  discount: "5 % off",
+};
+
+console.log(product);
---
+++ lecture 1/practice2.js
@@ -0,0 +1,15 @@
+const profile = {
+  username: "shardha Khapra",
+  followers: "345k",
+  following: "100",
+  posts: "50",
+  bio: "Web Developer and Educator",
+  location: "India",
+  skills: ["JavaScript", "React", "Node.js"],
+  isVerified: true,
+};
+
+console.log("Username:", profile.username);
+console.log("Followers:", profile.followers);
+console.log(typeof profile["isVerified"]);
+console.log(profile);
