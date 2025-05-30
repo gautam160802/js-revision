d681f4e47a54c702c72de7dd75bea9bd01f89046
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-05-30 13:14:20 +0530

    complete lecture 3

--- lecture 3/loop.js
+++ lecture 3/loop.js
@@ -1,7 +1,52 @@
 // loop in js
-count = 20;
 
-for (let count = 1; count <= 10; count++) {
-  console.log("Gautam kumar");
+// for (let count = 1; count <= 10; count++) {
+//   console.log("Gautam kumar");
+// }
+// console.log("loop finished");
+
+// cal sum of 1 to 5 using for loop
+
+// let sum = 0;
+
+// for (let count = 1; count <= 5; count++) {
+//   sum = sum + count;
+// }
+// console.log(sum);
+
+// while loop in js
+
+// let count = 8;
+// while (count <= 10) {
+//   console.log("Gautam kumar");
+//   count++;
+// }
+
+// do while loop in js
+
+// let count = 1;
+// do {
+//   console.log("Gautam kumar");
+//   count++;
+// } while (count < 4);
+
+// loops in js
+
+// for of loop in js
+
+// let str = "Gautam Kumar";
+
+// for (let i of str) {
+//   console.log("i =>", i);
+// }
+// for in loop in js
+
+let student = {
+  name: "Gautam Kumar",
+  age: 22,
+  city: "Delhi",
+  isPass: true,
+};
+for (let i in student) {
+  console.log("i =>", i, "value =>", student[i]);
 }
-console.log("loop finished");
