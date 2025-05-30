34e90e737a2aa3c9feda428cc9a6c6862608e16e
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-05-30 22:07:52 +0530

    pracitcing about the function

--- lecture 6/function.js
+++ lecture 6/function.js
@@ -1,1 +1,34 @@
-console.log("Function");
+// // console.log("Function");
+// // let's study funcitons in JavaScript
+// // block of code that performs a specific task can be invoked when needed
+// // function defination
+
+// function greet(name) {
+//   console.log("Hello " + name);
+// }
+
+// // function invocation
+// greet("Gautam");
+// // function with return value
+
+// function add(a, b) {
+//   return a + b;
+// }
+// // function invocation with return value
+// let sum = add(5, 10);
+// console.log("sum is ", sum);
+
+// basic function example
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+// // function invocation with return value
+// let product = multiply(5, 10);
+// console.log("product is :", product);
+
+function divide(a, b) {
+  return a / b;
+}
+let quotient = divide(10, 2);
+console.log("quotient is: ", quotient);
