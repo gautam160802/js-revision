5b0a233bb2ff57623744193455366da38d004126
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-05-30 14:11:03 +0530

    complete lecture 4

--- lecture 4/string.js
+++ lecture 4/string.js
@@ -1,1 +1,44 @@
-console.log("Hello, World!");
+// string in js
+// create a string
+
+// let str = "Hello World!";
+
+// // string length
+// let strLength = str.length;
+// // string indeices
+
+// let firstChar = str[0];
+// let lastchar = str[str.length - 1];
+
+// console.log("string length:", strLength);
+// console.log(firstChar);
+// console.log(lastchar);
+
+// template literals in js:
+// let name = "Gautam";
+// let age = 20;
+// let str = `My name is ${name} and age is ${age}.`;
+// console.log(str);
+
+// string interpolation
+// to create strings by doing substitutions of placeholders
+
+// let str = "Hello World!";
+// let str2 = str.replace("World", "Gautam");
+// console.log(str2);
+
+// let str = "Hello$world";
+// let str2 = str.toUpperCase();
+// console.log(str2);
+// let str3 = str.toLowerCase();
+// console.log(str3);
+
+// let str4 = str.slice(5, 7);
+// console.log(str4);
+// let str5 = str.slice(6);
+// console.log(str5);
+
+// string methods
+let str = "Hello World!";
+let str2 = str.split(" "); // split the string into an array of substrings
+console.log(str2); // ["Hello", "World!"]
