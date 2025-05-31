ebc1b10f9a1a7eae08c4d3de4ae50a40241575bc
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-05-31 20:06:24 +0530

    This is basic understanding of of function

---
+++ lecture 6/class.js
@@ -0,0 +1,18 @@
+// function myFunction() {
+//   console.log("Hello Gautam");
+// }
+// myFunction();
+
+// function myFunction(msg) {
+//   console.log(msg);
+// }
+// myFunction("Hello Gautam");
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(5, 10));
+
+function sum(a, b) {
+  console.log(a + b);
+}
