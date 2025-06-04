3833105e4e10722dd47f23c50f2af999eb8d71ae
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-06-04 18:05:56 +0530

    This is the practice of DOM

--- lecture 7/DOM1.js
+++ lecture 7/DOM1.js
@@ -1,6 +1,9 @@
 // console.dir(document.body);
 // DOM Manipulation
 
-let headings = document.getElementsByClassName("heading-class");
-console.log(headings);
-console.dir(headings);
+// let headings = document.getElementsByClassName("heading-class");
+// console.log(headings);
+// console.dir(headings);
+
+let para = document.getElementsByTagName("p");
+console.dir(para);
