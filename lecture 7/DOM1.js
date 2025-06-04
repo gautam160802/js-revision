6b80e1e7f98485155bb7ead9c7d46ed6b98969f2
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-06-04 19:29:50 +0530

    pause at DOM

--- lecture 7/DOM1.js
+++ lecture 7/DOM1.js
@@ -5,5 +5,18 @@
 // console.log(headings);
 // console.dir(headings);
 
-let para = document.getElementsByTagName("p");
-console.dir(para);
+// let para = document.getElementsByTagName("p");
+// console.dir(para);
+
+let button = document.getElementById("myId");
+console.log(button);
+
+// let firstEl = document.querySelector("myClass");
+// console.dir(firstEl);
+
+// // this returns 1st element;
+
+// let allEl = document.querySelectorAll("myClass");
+// console.dir(allEl);
+
+// this returns all element;
